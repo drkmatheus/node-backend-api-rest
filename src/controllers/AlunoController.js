51a365189140976dac33e5d4a8562d6debd749bf
mcroscia <mcroscia@lifescan.com>
2025-09-26 14:14:52 -0300

    Implementação do CRUD completo para alunos, incluindo criação, leitura, atualização e exclusão, com validações e tratamento de erros.

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -6,6 +6,97 @@ class AlunoController {
 
     res.json(alunos);
   }
+
+  async create(req, res) {
+    try {
+      const novoAluno = await Aluno.create(req.body);
+      const { id, nome, sobrenome, email } = novoAluno;
+      return res.json({ id, nome, sobrenome, email });
+    } catch (e) {
+      return res
+        .status(400)
+        .json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["ID não enviado"],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não encontrado"],
+        });
+      }
+      return res.json(aluno);
+    } catch (e) {
+      return res
+        .status(400)
+        .json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["ID não enviado"],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não encontrado"],
+        });
+      }
+      const novo = await Aluno.update(id);
+      return res.json(novo);
+    } catch (e) {
+      return res
+        .status(400)
+        .json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["ID não enviado"],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não encontrado"],
+        });
+      }
+      await aluno.destroy(id);
+      return res.json(
+        `Aluno ${aluno.nome} de ID ${aluno.id} apagado da base de dados`
+      );
+    } catch (e) {
+      // Verificação de segurança ↓
+      const errorMessages = e.errors
+        ? e.errors.map((err) => err.message)
+        : [e.message]; // Captura mensagens genéricas
+
+      return res.status(400).json({ errors: errorMessages });
+    }
+  }
 }
 
 export default new AlunoController();
--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -4,12 +4,57 @@ export default class Aluno extends Model {
   static init(sequelize) {
     super.init(
       {
-        nome: Sequelize.STRING,
-        sobrenome: Sequelize.STRING,
-        email: Sequelize.STRING,
-        idade: Sequelize.INTEGER,
-        peso: Sequelize.FLOAT,
-        altura: Sequelize.FLOAT,
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          validate: {
+            len: { args: [3, 30], msg: "Campo nome de tamanho inválido" },
+          },
+        },
+        sobrenome: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          validate: {
+            len: { args: [3, 30], msg: "Campo sobrenome de tamanho inválido" },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          validate: {
+            isEmail: { msg: "Campo email inválido" },
+          },
+          unique: {
+            msg: "Email em uso",
+          },
+        },
+        idade: {
+          type: Sequelize.INTEGER,
+          defaultValue: "",
+          validate: {
+            isInt: {
+              msg: "Idade inválida",
+            },
+          },
+        },
+        peso: {
+          type: Sequelize.FLOAT,
+          defaultValue: "",
+          validate: {
+            isFloat: {
+              msg: "Peso inválido",
+            },
+          },
+        },
+        altura: {
+          type: Sequelize.FLOAT,
+          defaultValue: "",
+          validate: {
+            isFloat: {
+              msg: "Altura inválida",
+            },
+          },
+        },
       },
       {
         sequelize,
