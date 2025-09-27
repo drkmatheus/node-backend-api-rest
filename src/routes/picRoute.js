d6d8fe67916243ef46424ae07ce60b5cefe74a4c
mcroscia <mcroscia@lifescan.com>
2025-09-27 19:01:08 -0300

    Adicionado filtro de tipo de arquivo para upload de imagens e ajustada a lógica de tratamento de erros no controller de fotos

--- src/routes/picRoute.js
+++ src/routes/picRoute.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import multer from "multer";
+
 import picController from "../controllers/PicController";
-import multerConfig from "../config/multerConfig";
 
 const router = Router();
-const upload = multer(multerConfig);
 
-router.post("/", upload.single("pic"), picController.save);
+router.post("/", picController.save);
 
 export default router;
